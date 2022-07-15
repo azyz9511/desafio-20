8bc169368957f7816c13e965be2e0263920a74f0
Juan David Perez <juandwow@gmail.com>
2022-07-14 23:29:46 -0300

    Desafio 20

---
+++ routes/indexRouter.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const connectMongo = require('connect-mongo');
+const passport = require('../DB/configPassport');
+require('dotenv').config();
+
+router.use(cookieParser());
+router.use(session({
+  store: connectMongo.create({
+    mongoUrl: `mongodb+srv://${process.env.USERNAMEDB}:${process.env.PASSWORDDB}@cluster0.33nzl.mongodb.net/${process.env.SESSIONSDB}?retryWrites=true&w=majority`,
+    mongoOptions: {useNewUrlParser: true, useUnifiedTopology: true},
+    ttl: 600
+  }),
+  secret: `${process.env.SECRETDB}`,
+  resave: true,
+  saveUninitialized: true
+}));
+router.use(passport.initialize());
+router.use(passport.session());
+
+
+// Ruta--------------------------------------------------------------------
+router.get('/',(req, res) => {
+  if(req.isAuthenticated()){
+    res.render('pages/index',{nombre : req.session.passport.user});
+  }else{
+    res.redirect('/login');
+  }
+});
+
+module.exports = router;
